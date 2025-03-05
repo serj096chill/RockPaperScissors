ef68be21bce2537ee614f2da09df53e1be9fddd2
Serj Chilingirian <serj.chilingirian@gmail.com>
2025-03-05 21:53:23 +0200

    create function to output randomly rock, paper or scissors

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ console.log(getComputerChoice());
 
 // Functions
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
+    let num = Math.floor(Math.random() * 3) + 1;
+    let word;
+    if (num === 1) {
+        word = "Rock";
+    } else if (num === 2) {
+        word = "Paper";
+    } else {
+        word = "Scissors";
+    }
+
+    return word
 }
